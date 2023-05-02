809a7615cde64664a33c8a38741d716fc08563f3
Surajkumarbehera <60914796+Surajkumarbehera@users.noreply.github.com>
2023-05-02 12:33:36 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { ResgistrationComponent } from "./resgistration/resgistration.component";
+
+const routes: Routes = [
+  {
+    path:'LoginPage',
+    component:LoginComponent 
+  },
+  {
+    path:'ResgistrationPage',
+    component:ResgistrationComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/resgistration/resgistration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resgistration',
+  templateUrl: './resgistration.component.html',
+  styleUrls: ['./resgistration.component.css']
+})
+export class ResgistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
