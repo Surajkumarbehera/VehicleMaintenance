96c776680808b28be31540bb77dcb3c6b5141b7c
Surajkumarbehera <60914796+Surajkumarbehera@users.noreply.github.com>
2023-06-08 12:12:00 +0530

    Update productService.ts

--- src/app/productService.ts
+++ src/app/productService.ts
@@ -19,18 +19,23 @@ export class ProductService{
    loginUser(userObj: { username: any; password: any; }):Observable<Object>
     {
         console.log('loginObject',  userObj);
-        return this._httpClient.post('http://localhost:9090/myapp/user/login', JSON.stringify(userObj),{headers:this.httpHeaders}); 
+       return this._httpClient.post('http://ivms:9090/myapp/user/login', JSON.stringify(userObj),{headers:this.httpHeaders});
+     //   return this._httpClient.post('http://localhost:9090/myapp/user/login', JSON.stringify(userObj),{headers:this.httpHeaders}); 
     }
 
     registerUser(userProfileObj : any):Observable<Object>
     {
         console.log('registrationObject:', userProfileObj);
-        return this._httpClient.post('http://localhost:9090/myapp/user/register', JSON.stringify(userProfileObj),
-    {headers:this.httpHeaders});
+      return this._httpClient.post('http://ivms:9090/myapp/user/register', JSON.stringify(userProfileObj), {headers:this.httpHeaders});
+       // return this._httpClient.post('http://localhost:9090/myapp/user/register', JSON.stringify(userProfileObj), {headers:this.httpHeaders});
     }
     getUserDeatils(userId:String):Observable<Object>
     {   
-        return this._httpClient.post('http://localhost:9090/myapp/user/data',{params:{'user_id':userId}});
+      return this._httpClient.post('http://ivms:9090/myapp/user/data',{params:{'user_id':userId}});
+        //return this._httpClient.post('http://localhost:9090/myapp/user/data',{params:{'user_id':userId}});
         //return this._httpClient.post('https://run.mocky.io/v3/677ada57-1641-43fa-83e8-219082959bc0',{headers:this.httpHeaders});
+     
+
+
     }
 }
